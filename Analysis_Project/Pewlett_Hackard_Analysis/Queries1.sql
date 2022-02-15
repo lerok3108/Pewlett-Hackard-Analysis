0930669e796901beabf6ef1237e3efc1a0287821
lerok3108 <lera.hamm@yahoo.com>
2022-02-15 16:13:54 -0500

    commit queries.

--- Analysis_Project/Pewlett_Hackard_Analysis/Queries1.sql
+++ Analysis_Project/Pewlett_Hackard_Analysis/Queries1.sql
@@ -17,6 +17,7 @@ WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
 
 --How many employees were born in 1954
 SELECT first_name, last_name
+
 FROM employees
 WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
 
@@ -47,3 +48,124 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 --Check the results of previous 
 SELECT * FROM retirement_info;
+
+--Recreate retiremen_info table to include employee numbers
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_number = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_employees.to_date
+FROM retirement_info
+LEFT JOIN dept_employees
+ON retirement_info.emp_no = dept_employees.emp_no;
+
+--Use aliases for code readability
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+--Use Left Join for retirement_info and dept_emp tables to ensure they all still work there
+--Resulting table contains only current employees
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retiring_by_department
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--Create employee information list
+SELECT * FROM salaries;
+
+--what the most recent date on this list is, so let's sort that column in descending order
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no=s.emp_no)
+INNER JOIN dept_employees as de
+ON (e.emp_no=de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+
+-- List of retiring managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_number)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+--Create list of department retirees
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_number);
+
+--CReate list of sales department retirees
+SELECT * 
+INTO sales_department
+FROM dept_info
+WHERE dept_name='Sales';
+
+--Create list of retirees in Sales and Development teams
+SELECT * 
+INTO sales_dev
+FROM dept_info
+WHERE dept_name IN ('Sales','Development');
+
